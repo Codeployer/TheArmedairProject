300f24e485ac98b3f5a6371faf79fff92f814365
Hubert Lewandowski <lewandowski.hubert486@gmail.com>
2017-05-21 16:12:02 +0200

    Add project files.

---
+++ TheArmedairProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TheArmedairProject.Startup))]
+namespace TheArmedairProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
