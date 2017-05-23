1e2dd466358b76e391e2a283882cf2117265cf19
Hubert Lewandowski <lewandowski.hubert486@gmail.com>
2017-05-23 17:44:18 +0200

    Footer changes, "Add Page" option is now funtional,  new content - anyway ultra bugged

--- TheArmedairProject/Controllers/AdminPanelController.cs
+++ TheArmedairProject/Controllers/AdminPanelController.cs
@@ -13,5 +13,10 @@ namespace TheArmedairProject.Controllers
         {
             return View();
         }
+
+        public ActionResult Pages()
+        {
+            return View();
+        }
     }
 }
